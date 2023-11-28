ba6dd221b4f3995f546731e234909e48a48fa45e
matygullinruslan <matygullinruslan@gmail.com>
2023-11-29 00:17:09 +0300

    Удалил лишние файлы

--- tests/qa_demo_9.5.py
+++
@@ -1,7 +0,0 @@
-from selene import browser
-
-browser.open('https://sptlab.ru/')
-browser.element('#sbs-587027710-1683233369309').click()
-browser.element('.t794__arrow').hover()
-browser.element('. t-name_xs').click()
-...
